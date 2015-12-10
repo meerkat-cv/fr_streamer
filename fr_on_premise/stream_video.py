265a2c4d75b70bebb50e58f0506389622f9cf353
Gustavo Führ <gustavofuhr87@gmail.com>
2015-12-10 00:04:00 -0200

    Change to tornado in server (much better)

--- fr_on_premise/stream_video.py
+++ fr_on_premise/stream_video.py
@@ -1,21 +1,27 @@
 
 import cv2
 from fr_on_premise import frame_pb2
+import websocket
 
 
 class StreamVideo:
 
 	def __init__(self, video_filename, ws_url):
 		self.video = cv2.VideoCapture(video_filename)
-		self.ws_url = ws_url
+		self.ws = websocket.create_connection("ws://localhost:8000/echo", subprotocols=["binary", "base64"])
 
 	def stream(self):
 
 		while True:
 			ret, frame = self.video.read()
 			f = frame_pb2.Frame()
-			f.cols = 200
-			f.rows = 200
-			f.data = bytes(frame)
-			cv2.imshow("hey", frame)
-			cv2.waitKey(10)
+			f.rows = frame.shape[0]
+			f.cols = frame.shape[1]
+			f.data = bytes(frame.tobytes())
+			fstr = f.SerializeToString()
+			print(fstr)
+			print(type(fstr))
+			self.ws.send(fstr)
+
+			# cv2.imshow("hey", frame)
+			cv2.waitKey(30000)
--- tests/mock_server/websocket_server.py
+++ tests/mock_server/websocket_server.py
@@ -1,32 +1,56 @@
-from flask import Flask
-from flask_sockets import Sockets
+import tornado
 import numpy as np
 import cv2
+import frame_pb2
+import tornado.websocket 
 
-app = Flask(__name__)
-sockets = Sockets(app)
+PORT = 8000
 
-@sockets.route('/echo')
-def echo_socket(ws):
-    while True:
-        message = ws.receive()
-        arr = np.asarray(bytearray(message), dtype=np.uint8)
-        arr = np.reshape(arr, (256,256))
-        print(arr)
-        image = arr
-        #cv2.imdecode(arr, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-        height, width = image.shape[:2]
-        print(height)
-        print(width)
-        cv2.imwrite('received.jpg', image)
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
 
-        # cv2.waitKey()
-        print('written.')
-        ws.send("I received this")
+    def open(self):
+        print("WebSocket opened")
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+    def on_message(self, message):
+        print("You said: ")
+        print(message)
+        f = frame_pb2.Frame()
+        f.ParseFromString(message)
+        print(f)
+
+    def on_close(self):
+        print("WebSocket closed")
 
 
-#app.run()
+application = tornado.web.Application(
+    [
+        (r"/echo", EchoWebSocket),
+    ]
+)
+ 
+if __name__ == "__main__":
+    application.listen(PORT)
+    print("Running app on port {}".format(PORT))
+    tornado.ioloop.IOLoop.instance().start()
+        # message = ws.receive()
+        # print('message!', message)
+        # f = frame_pb2.ParseFromString(message)
+        # print(f)
+
+
+        # arr = np.asarray(bytearray(message), dtype=np.uint8)
+        # arr = np.reshape(arr, (256,256))
+        # print(arr)
+        # image = arr
+        # #cv2.imdecode(arr, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        # height, width = image.shape[:2]
+        # print(height)
+        # print(width)
+        # cv2.imwrite('received.jpg', image)
+
+        # # cv2.waitKey()
+        # print('written.')
+        # ws.send("I received this")
+
