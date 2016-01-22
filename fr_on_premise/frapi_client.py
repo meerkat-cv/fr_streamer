0eec7f368aa926dd9e607a170de11729065fb317
Guilherme Fickel <guilhermefickel@gmail.com>
2016-01-22 13:40:23 -0200

    Frapi client is the one responsible for showing the results. Also changed the timeout to 5 seconds, since there is some synch problems when it is triggered due to slow processing and not from missing response.

--- fr_on_premise/frapi_client.py
+++ fr_on_premise/frapi_client.py
@@ -7,6 +7,7 @@ class FrapiClient():
     def __init__(self):
         self.ioloop = ioloop.IOLoop.instance()
         self.streams = []
+        self.window_name = []
         def ioloop_fun(self):
             try:
                 ioloop.IOLoop.instance().start()
@@ -17,10 +18,42 @@ class FrapiClient():
         t.start()
 
 
-    def transmit(self, video_name, ws_url):
+    def transmit(self, window_name, video_name, ws_url):
+        self.window_name.append(window_name)
+        stream_id = len(self.window_name)-1
         stream = StreamVideo()
-        stream.config(video_name, ws_url)
+        stream.config(video_name, ws_url, stream_id, self)
         self.streams.append(stream)
+
+
+
+    def on_message(self, image, ores, stream_id):
+        if image is not None and 'people' in ores:
+            self.plot_recognition_info(image, ores, stream_id)
+
+
+    def plot_recognition_info(self, image, ores, stream_id):
+        for rec in ores['people']:
+            l = rec['top_left']['x']
+            t = rec['top_left']['y']
+            r = rec['bottom_right']['x']
+            b = rec['bottom_right']['y']
+            tl = [l, t]
+            br = [r, b]
+            cv2.rectangle(image, (int(tl[0]), int(tl[1])), (int(br[0]), int(br[1])), (165, 142, 254), 4)
+            
+            font_face = cv2.FONT_HERSHEY_SIMPLEX
+            label = rec["recognition"]["predictedLabel"]
+            
+            (text_size, baseline) = cv2.getTextSize(label, font_face, 1, 2);
+            text_pt = (int(tl[0]), int(tl[1])-10)
+            cv2.rectangle(image, (text_pt[0], text_pt[1]-text_size[1]-4), (text_pt[0] + text_size[0], text_pt[1]+4), (165, 142, 254), -1)
+            cv2.putText(image, label, text_pt, font_face, 1, (255, 255, 255), 2)
+
+
+        cv2.imshow(self.window_name[stream_id], image)
+        cv2.waitKey(1)
+        
 
 
     def end_transmissions(self):
--- fr_on_premise/stream_video.py
+++ fr_on_premise/stream_video.py
@@ -11,18 +11,13 @@ class StreamVideo(WebSocketClient):
         self.connect_timeout = DEFAULT_CONNECT_TIMEOUT
         self.request_timeout = DEFAULT_REQUEST_TIMEOUT
         self.time_out_stream = None
+        self.original_frame = None
+        self.stream_id = -1
         
 
     def _on_message(self, msg):
-        print('Reading message')
-        print(msg)
-        # ores = json.loads(msg)
-        # if len(ores) > 0:
-        #     self.plot_recognition_info(self.original_frame, ores)
-        
-        # cv2.putText(self.original_frame, sfps, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness = 2)
-        # cv2.imshow("Candiru follows the stream", cv2.resize(self.original_frame, (0,0), fx=0.3, fy=0.3))
-        # cv2.waitKey(1)
+        ores = json.loads(msg)
+        self.client.on_message(self.original_frame, ores, self.stream_id)
         self.call_stream()
 
 
@@ -31,12 +26,14 @@ class StreamVideo(WebSocketClient):
             ioloop.IOLoop().instance().remove_timeout(self.time_out_stream)
 
         self.stream()
-        deadline = time.time() + 1
+        deadline = time.time() + 5 # 5 seconds seems good enough
         self.time_out_stream = ioloop.IOLoop().instance().add_timeout(deadline, self.call_stream)
 
     
-    def config(self, stream_path, ws_url):
+    def config(self, stream_path, ws_url, stream_id, client):
         self.video = cv2.VideoCapture(stream_path)
+        self.client = client
+        self.stream_id = stream_id
         if self.video.isOpened() == False:
             print('problem opening', stream_path)
             return
@@ -58,31 +55,13 @@ class StreamVideo(WebSocketClient):
         self.stream()
 
 
-    def plot_recognition_info(self, image, ores):
-        for rec in ores['people']:
-            l = rec['top_left']['x']
-            t = rec['top_left']['y']
-            r = rec['bottom_right']['x']
-            b = rec['bottom_right']['y']
-            tl = [l, t]
-            br = [r, b]
-            cv2.rectangle(image, (int(tl[0]), int(tl[1])), (int(br[0]), int(br[1])), (165, 142, 254), 4)
-            
-            font_face = cv2.FONT_HERSHEY_SIMPLEX
-            label = rec["recognition"]["predictedLabel"]
-            
-            (text_size, baseline) = cv2.getTextSize(label, font_face, 1, 2);
-            text_pt = (int(tl[0]), int(tl[1])-10)
-            cv2.rectangle(image, (text_pt[0], text_pt[1]-text_size[1]-4), (text_pt[0] + text_size[0], text_pt[1]+4), (165, 142, 254), -1)
-            cv2.putText(image, label, text_pt, font_face, 1, (255, 255, 255), 2)
-
-
     @gen.coroutine
     def stream(self):
         print('streaming!!!')
         ret, self.original_frame = self.video.read()
 
         if self.original_frame is None:
+            print('Closing')
             self.close()
             return
 
