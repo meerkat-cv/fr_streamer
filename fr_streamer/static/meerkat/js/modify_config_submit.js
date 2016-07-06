ec3acf5eadf73277b03408bff510baebd148ee37
Gustavo Führ <gustavofuhr87@gmail.com>
2016-07-06 14:24:54 -0300

    Made the model of js which we should develop on top

--- fr_streamer/static/meerkat/js/modify_config_submit.js
+++ fr_streamer/static/meerkat/js/modify_config_submit.js
@@ -1,16 +1,50 @@
-var name=document.getElementById( "name_of_user" );
-var age=document.getElementById( "age_of_user" );
-var course=document.getElementById( "course_of_user" );
+(function (global, $) {
 
-$.ajax({
-  type: 'post',
-  url: 'insertdata.php',
-  data: {
-    user_name:name,
-    user_age:age,
-    user_course:course
-  },
-  success: function (response) {
-    $('#success__para').html("You data will be saved");
-  }
-});
+    var ModifyConfig = {};
+
+    ModifyConfig.init = function () {
+        this.bindSubmitButton();
+    }
+
+    ModifyConfig.getData = function () {
+        d = {
+            'api_key': $('#apiKeyInput').val,
+            'ip': $('#ipInput').val,
+            'port': parseInt($('#portInput').val, 10),
+            'output': {}
+        };
+
+        if ($('#jsonCheck').checked) {
+            d['output']['json'] = {
+                'node_frames': parseInt($('#jsonNode').val, 10),
+                'dir': $('#jsonSaveDir').val
+            };
+        }
+        if ($('#postHttpCheck').checked) {
+            d['output']['json'] = {
+                'node_frames': parseInt($('#jsonNode').val, 10),
+                'dir': $('#jsonSaveDir').val
+            };
+        }
+    }
+
+    ModifyConfig.bindSubmitButton = function () {
+
+        $.ajax({
+            type: 'post',
+            url: 'insertdata.php',
+            data: {
+                user_name: name,
+                user_age: age,
+                user_course: course
+            },
+            success: function (response) {
+                $('#success__para').html("You data will be saved");
+            }
+        });
+    }
+
+    global.ModifyConfig = new ModifyConfig();
+    global.ModifyConfig.init();
+
+}(window, jQuery));
